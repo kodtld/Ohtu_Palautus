99c98fef79073cfb1565f4fb1b336bdd5acba1d5
kodtld <kodu@live.fi>
2022-11-21 21:09:58 +0200

    2

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -1,12 +1,12 @@
 HINTA = 5
 
-
 class Kassapaate:
     def __init__(self):
         self.myytyja_lounaita = 0
 
     def lataa(self, kortti, summa):
-        kortti.lataa(summa)
+        if summa > 0:
+            kortti.lataa(summa)
 
     def osta_lounas(self, kortti):
         if kortti.saldo >= 5:
--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ladataan_positiivinen_summa(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock,5)
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    def test_kortille_ei_ladata_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock,-2)
+        maksukortti_mock.lataa.assert_not_called()
